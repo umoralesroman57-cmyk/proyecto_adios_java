c381429782375e2fd3c1b6b77610007d252a06c4
umoralesroman57-cmyk <umoralesroman57@gmail.com>
2026-05-13 12:46:18 -0600

    programa que suma dos números

--- AdiosJava.java
+++ AdiosJava.java
@@ -1,7 +1,13 @@
-
+import java.util.Scanner;
 public class AdiosJava {
     public static void main(String[] args) {
-        System.out.println("Adios java");
-        System.out.println("Comenzaremos a usar Python");
+        int numero1, numero2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escribe el numero 1: ");
+        numero1 = scanner.nextInt();
+        System.out.println("Escribe el numero 2: ");
+        numero2 = scanner.nextInt();
+        int resultado = numero1 + numero2;
+        System.out.println("La suma es: " + resultado);
     }
 }
